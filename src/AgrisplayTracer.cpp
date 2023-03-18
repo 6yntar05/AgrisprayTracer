3eaa0fa093834a4a994792def7561398b93a31f5
6yntar05 <6yntar05@mail.ru>
2023-03-18 20:31:03 +0700

    I hate 🤬ing cmake

---
+++ src/AgrisplayTracer.cpp
@@ -0,0 +1,31 @@
+// parsekml.cc
+// This program parses a KML Placemark from a memory buffer and prints
+// the value of the <name> element on standard output.
+
+#include <iostream>
+#include <string>
+#include "kml/dom.h"  // The KML DOM header.
+
+int main() {
+  // Parse KML from a memory buffer.
+  std::string errors;
+  kmldom::ElementPtr element = kmldom::Parse(
+    "<kml>"
+      "<Placemark>"
+        "<name>hi</name>"
+        "<Point>"
+          "<coordinates>1,2,3</coordinates>"
+        "</Point>"
+      "</Placemark>"
+    "</kml>",
+    &errors);
+
+  // Convert the type of the root element of the parse.
+  const kmldom::KmlPtr kml = kmldom::AsKml(element);
+  const kmldom::PlacemarkPtr placemark =
+    kmldom::AsPlacemark(kml->get_feature());
+
+  // Access the value of the <name> element.
+  std::cout << "The Placemark name is: " << placemark->get_name()
+    << std::endl;
+}
